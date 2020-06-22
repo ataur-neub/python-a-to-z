fdf2a55bca2650ac03d70ac6f0177bb10174aca5
Ataur Rahman Selim <37262722+ataur-neub@users.noreply.github.com>
2020-06-22 08:36:29 +0600

    first program done

---
+++ dynamic_input.py
@@ -0,0 +1,6 @@
+//in python you do not need to declare datatype
+//input from user
+userName=input("Enter any input: ")
+print(f"hello { userName }!") //whatever you will take input here it will printed, here f means format
+//another way to print output
+print("hello {}".format(username))
